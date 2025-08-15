65a20382814ed477afc2c172917dfdfc4f7e5bd1
ShaneS02 <shanematthewskinner@gmail.com>
2025-08-15 02:37:44 -0400

    Beginning to Implement data Scraping
    Gathered a list of websites to scrape and loading the embeddings of the information into the database

--- nextjs-gpt/scripts/loadDb.ts
+++ nextjs-gpt/scripts/loadDb.ts
@@ -3,4 +3,106 @@ import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/we
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 import { InferenceClient } from "@huggingface/inference";
 
-import "dotenv/config"
+import "dotenv/config" //allows us to access values in the .env file 
+import { SimilarityMetric } from "@langchain/community/vectorstores/rockset";
+
+type similarityMetric = "dot_product" | "cosine" | "euclidean"
+
+const {
+    ASTRA_DB_NAMESPACE,
+    ASTRA_DB_COLLECTION,
+    ASTRA_DB_API_ENDPOINT,
+    ASTRA_DB_APPLICATION_TOCKET,
+    HUGGINGFACE_API_KEY
+} = process.env
+
+const huggingface = new InferenceClient(HUGGINGFACE_API_KEY)
+const embeddingModel = 'sentence-transformers/all-MiniLM-L12-v2';
+
+const data = [
+    'https://en.wikipedia.org/wiki/List_of_animal_names',
+    'https://en.wikipedia.org/wiki/List_of_mammals_of_Africa',
+    'https://en.wikipedia.org/wiki/List_of_domesticated_animals',
+    'https://en.wikipedia.org/wiki/List_of_largest_organisms',
+    'https://en.wikipedia.org/wiki/List_of_longest-living_organisms',
+    'https://en.wikipedia.org/wiki/List_of_mammals_of_Canada',
+    'https://en.wikipedia.org/wiki/List_of_mammals_of_Australia',
+    'https://en.wikipedia.org/wiki/List_of_birds_of_India',
+    'https://en.wikipedia.org/wiki/List_of_reptiles_of_South_Africa',
+    'https://en.wikipedia.org/wiki/List_of_mammals_of_the_United_States',
+    'https://en.wikipedia.org/wiki/List_of_birds_of_the_United_States',
+    'https://en.wikipedia.org/wiki/List_of_mammals_of_Asia',
+    'https://en.wikipedia.org/wiki/List_of_birds_of_Asia'
+]
+
+const client = new DataAPIClient(ASTRA_DB_APPLICATION_TOCKET)
+const db = client.db(ASTRA_DB_API_ENDPOINT, { namespace: ASTRA_DB_NAMESPACE })
+
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 512,
+    chunkOverlap: 100
+})
+
+const createCollection = async (SimilarityMetric: similarityMetric = "dot_product") => {
+    const res = await db.createCollection(ASTRA_DB_COLLECTION, {
+        vector: {
+            dimension: 768, //this is for the huggingface model sentence-transformers/all-Mpnet-base-v2. If too slow, can swap to sentence-transformers/all-MiniLM-L12-v2 (dimensions: 384)
+            metric: SimilarityMetric
+        }
+    })
+    console.log(res) // output response
+}
+
+const loadSampleData = async () => {
+    const collection = await db.collection(ASTRA_DB_COLLECTION)
+
+    for await (const url of data) {
+        const content = await scrapePage(url)
+        const chunks = await splitter.splitText(content)
+        for await (const chunk of chunks) {
+            const vector = async function getEmbeddings(chunk: string): Promise<number[]> {
+                const response = await fetch(
+                    `https://api-inference.huggingface.co/models/${embeddingModel}`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "Authorization": `Bearer ${HUGGINGFACE_API_KEY}`,
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ inputs: chunk }),
+                    }
+                );
+
+                const data = await response.json();
+                return data[0].embedding;
+            }
+
+            const res = await collection.insertOne({
+                $vector: vector,
+                text: chunk
+            })
+
+        }
+    }
+}
+
+const scrapePage = async (url: string) => {
+    const loader = new PuppeteerWebBaseLoader(url, {
+        launchOptions: {
+            headless: true //Note: check this if there is an issue
+        },
+        gotoOptions: {
+            waitUntil: "domcontentloaded"
+        },
+        evaluate: async (page, browser) => {
+            const result = await page.evaluate(() => document.body.innerHTML)
+            await browser.close()
+            return result
+        }
+
+
+    })
+    return (await loader.scrape())?.replace(/<[^>]*>?/gm, '') //only interested in the text. Remove tags
+}
+
+createCollection().then(() => loadSampleData())
